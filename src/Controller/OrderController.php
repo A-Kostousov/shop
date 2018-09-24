8aafb100418ff217d3075c5eb511c239ac6ac0e9
sqatio <sqatio@gmail.com>
2018-09-24 21:28:19 +0300

    Урок 21. Корзина/xdebug

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Service\Orders;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderController extends AbstractController
+{
+    /**
+     * @Route("/order", name="order")
+     */
+    public function index()
+    {
+        return $this->render('order/index.html.twig', [
+            'controller_name' => 'OrderController',
+        ]);
+    }
+
+    /**
+     * @Route ("/orders/add-to-cart/{id}/{quantity}", name="orders_add_to_cart")
+     *
+     * @throws
+     */
+    public function addToCart(Product $product, Orders $orders, Request $request, $quantity = 1)
+    {
+        $orders->addToCart($product, $quantity);
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
